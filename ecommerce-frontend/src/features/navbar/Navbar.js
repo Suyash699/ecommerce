9fb54cdac10e2321e4abf0588ed29a15fa61e877
Suyash Pandey <psuyash17.sp@gmail.com>
2023-10-09 09:07:23 +0530

    added cart with redux

--- ecommerce-frontend/src/features/navbar/Navbar.js
+++ ecommerce-frontend/src/features/navbar/Navbar.js
@@ -5,6 +5,7 @@ import {
   ShoppingCartIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const user = {
   name: "Tom Cook",
@@ -65,6 +66,7 @@ function Navbar({ children }) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
+                    <Link to="/cart">
                       <button
                         type="button"
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -75,6 +77,7 @@ function Navbar({ children }) {
                           aria-hidden="true"
                         />
                       </button>
+                      </Link>
                       <span className="inline-flex items-center rounded-md bg-red-50 mb-7 -ml-3 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset z-40 ring-red-600/10">
                         3
                       </span>
